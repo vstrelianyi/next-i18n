dc42c8aba90b336d246c623e0183094652c2e86c
viktor_strelianyi <chmpain@gmail.com>
2025-09-16 09:14:17 +0300

    - udpate

--- components/LocaleSwitcher.tsx
+++ components/LocaleSwitcher.tsx
@@ -1,5 +1,6 @@
 import {useLocale, useTranslations} from 'next-intl';
 import {routing} from '@/i18n/routing';
+
 import LocaleSwitcherSelect from '@/components/LocaleSwitcherSelect';
 
 export default function LocaleSwitcher() {
